2c057b8f34b1d15e4a74483f7368e54bd7e97af3
wangqingchen15369 <wangqingchen15369@ipalfish.com>
2021-09-26 14:36:47 +0800

    jj

---
+++ code/top100/k个一组反转链表.go
@@ -0,0 +1,37 @@
+package main
+
+import algorithm "algorithm/code/const"
+
+func reverseKGroup(head *algorithm.ListNode, k int) *algorithm.ListNode {
+	hair := &algorithm.ListNode{Next: head}
+	pre := hair
+
+	for head != nil {
+		tail := pre
+		for i := 0; i < k; i++ {
+			tail = tail.Next
+			if tail == nil {
+				return hair.Next
+			}
+		}
+		nex := tail.Next
+		head, tail = myReverse(head, tail)
+		pre.Next = head
+		tail.Next = nex
+		pre = tail
+		head = tail.Next
+	}
+	return hair.Next
+}
+
+func myReverse(head, tail *algorithm.ListNode) (*algorithm.ListNode, *algorithm.ListNode) {
+	prev := tail.Next
+	p := head
+	for prev != tail {
+		nex := p.Next
+		p.Next = prev
+		prev = p
+		p = nex
+	}
+	return tail, head
+}
